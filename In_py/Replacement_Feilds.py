d0e1c2c3095f02eaa00cb43453c5031b189d81fe
Zuriel_Nicholas <2001555@sit.singaporetech.edu.sg>
2022-05-29 20:17:14 +0800

    Update Replacement_Feilds.py

--- In_py/Replacement_Feilds.py
+++ In_py/Replacement_Feilds.py
@@ -28,3 +28,12 @@ for i in range(1, 13):
 print("\nCenter alignment")
 for i in range(1, 13):
     print("No.{0:^2} square is {1:^4} and cubed is {2:^4}".format(i, i**2, i**3))
+print("\n\n\n")
+# Accuracy
+print('Pi is approximately {0:12}'.format(22/7))
+print('Pi is approximately {0:12f}'.format(22/7))
+print('Pi is approximately {0:12.50f}'.format(22/7))
+print('Pi is approximately {0:52.50f}'.format(22/7))
+print('Pi is approximately {0:62.50f}'.format(22/7))
+print('Pi is approximately {0:72.50f}'.format(22/7))
+print('Pi is approximately {0:<72.50f}'.format(22/7))
