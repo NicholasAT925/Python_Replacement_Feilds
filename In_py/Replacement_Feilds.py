88d28ec7f3a75ec2af9610ecfe7e5ded082573a3
Zuriel_Nicholas <2001555@sit.singaporetech.edu.sg>
2022-05-29 20:11:36 +0800

    format

--- In_py/Replacement_Feilds.py
+++ In_py/Replacement_Feilds.py
@@ -10,11 +10,21 @@ print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}. My age is {8}
 .format(31, "Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec", age))
 # python does all the concatination automatically you can also manipulate the 
 # the order the concatination happens like so bellow.
+
 print("My name is {3}, sounds like {3}. Age {0} so i was born {1} and lived to be {2}"
     .format(24, 1990, "an immortal", "BOB")) 
     # The arguments in format can be taken as .format(0,1,2,3) like an index
     # Thus python replaces them to the coresponding numbers in the print function text
-print("\n")
+print("\n")# blank line
+
 # We can also format the the printing function so that it appears better
 for i in range(1, 13):
     print("No.{0:2} square is {1:4} and cubed is {2:4}".format(i, i**2, i**3))
+# left alignment
+print("\nLeft alignment")
+for i in range(1, 13):
+    print("No.{0:<2} square is {1:<4} and cubed is {2:<4}".format(i, i**2, i**3))
+# Center alignment
+print("\nCenter alignment")
+for i in range(1, 13):
+    print("No.{0:^2} square is {1:^4} and cubed is {2:^4}".format(i, i**2, i**3))
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python_Replacement_Feilds
  Learning to use replacement feilds in py
+ Learning to format text to appear better to users when reading the text
